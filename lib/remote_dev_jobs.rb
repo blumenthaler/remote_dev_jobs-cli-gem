2cbfdc0166055d048749537a9b252b660c0316dd
jsgoldb <jgoldb@yahoo.com>
2016-01-11 11:29:45 -0600

    changed file requirements

--- lib/remote_dev_jobs.rb
+++ lib/remote_dev_jobs.rb
@@ -1,7 +1,14 @@
 
-#require 'remote_dev_jobs'
-require 'require_all'
-require_all 'lib'
+
+
+require 'remote_dev_jobs/Launch.rb'
+require 'remote_dev_jobs/CLI.rb'
+require 'remote_dev_jobs/Job.rb'
+require 'remote_dev_jobs/AuthenticScraper.rb'
+require 'remote_dev_jobs/FlexJobsScraper.rb'
+require 'remote_dev_jobs/StackScraper.rb'
+require 'remote_dev_jobs/version.rb'
+require 'remote_dev_jobs/WeWorkRemotelyScraper.rb'
 
 
 module RemoteDevJobs
--- lib/remote_dev_jobs/version.rb
+++ lib/remote_dev_jobs/version.rb
@@ -1,3 +1,3 @@
 module RemoteDevJobs
-  VERSION = "0.1.7"
+  VERSION = "0.1.8"
 end
