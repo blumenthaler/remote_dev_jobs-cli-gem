68d833e2180d7786b6f4e646fda824c55850e4ec
jsgoldb <jgoldb@yahoo.com>
2016-01-11 12:14:38 -0600

    testing

--- lib/remote_dev_jobs.rb
+++ lib/remote_dev_jobs.rb
@@ -1,17 +1,17 @@
 
 
 
-# require 'remote_dev_jobs/Launch.rb'
-# require 'remote_dev_jobs/CLI.rb'
-# require 'remote_dev_jobs/Job.rb'
-# require 'remote_dev_jobs/AuthenticScraper.rb'
-# require 'remote_dev_jobs/FlexJobsScraper.rb'
-# require 'remote_dev_jobs/StackScraper.rb'
-# require 'remote_dev_jobs/version.rb'
-# require 'remote_dev_jobs/WeWorkRemotelyScraper.rb'
+require 'remote_dev_jobs/CLI.rb'
+require 'remote_dev_jobs/Launch.rb'
+require 'remote_dev_jobs/Job.rb'
+require 'remote_dev_jobs/AuthenticScraper.rb'
+require 'remote_dev_jobs/FlexJobsScraper.rb'
+require 'remote_dev_jobs/StackScraper.rb'
+require 'remote_dev_jobs/version.rb'
+require 'remote_dev_jobs/WeWorkRemotelyScraper.rb'
 
-require 'require_all'
-require_all 'lib'
+# require 'require_all'
+# require_all 'lib'
 
 
 module RemoteDevJobs
--- lib/remote_dev_jobs/version.rb
+++ lib/remote_dev_jobs/version.rb
@@ -1,3 +1,3 @@
 module RemoteDevJobs
-  VERSION = "0.1.8"
+  VERSION = "0.2.0"
 end
